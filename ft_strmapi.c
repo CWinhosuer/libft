c0bb4ee43a1bca19f01e76ad385f8a0c8661bd68
Kyle TWOMEY <ktwomey@e4r10p5.wethinkcode.co.za>
2018-06-14 14:30:36 +0200

    First Commit

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktwomey <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/31 08:35:06 by ktwomey           #+#    #+#             */
+/*   Updated: 2018/06/07 09:57:53 by ktwomey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *str, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*new;
+
+	i = 0;
+	if (!str || !f)
+		return (NULL);
+	new = (char*)malloc(sizeof(char) * (ft_strlen((char*)str) + 1));
+	if (!new)
+		return (NULL);
+	while (str[i])
+	{
+		new[i] = f(i, (char)str[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "libft.h"
+#include <stdio.h>
+
+int	main()
+{
+	printf("%s", ft_strtrim("   Hello   "));
+	return (0);
+}
+
